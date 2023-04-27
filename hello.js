655e61151db3651e45b00d4734b8defad6c308bf
Sarthak Kamble <43435014+Sarthak412@users.noreply.github.com>
2023-04-26 23:09:19 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -5,3 +5,11 @@ console.log("Welcome to JavaScript Programming"); // new commits
 
 console.log("Getting a merge request");
 
+function main(){
+  if(5 > 0){
+    console.log("Greater");
+  }
+  else{
+    console.log("Smaller");
+  }
+}
